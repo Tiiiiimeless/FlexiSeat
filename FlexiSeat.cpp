c54a12f416d862625df7c3ec8305073605a704a3
Tiiiiimeless <Hello_zzm@hotmail.com>
2026-04-04 10:35:34 +0800

    以3*6*3座位排布为例进行输出测试

--- FlexiSeat.cpp
+++ FlexiSeat.cpp
@@ -1,62 +1,36 @@
-﻿//#include <iostream>
-//#include <cstring>
-//
-//using namespace std;
-//
-//int row = 0, line = 0, column = 0;   //分别是列(row)，行(line)，排(column)
-//
-//struct student
-//{
-//	int coordinates_x;
-//	int coordinates_y;
-//	char studentName[100];
-//	string studentNumber;
-//};
-//
-//int main() {
-//	//for (int i = 0; i < 4; i++) {
-//	//	student student[i];
-//	//	string temporaryStudentNumber;
-//	//	cin >> temporaryStudentNumber;
-//	//	strcpy(student[i].studentNumber[i], temporaryStudentNumber);
-//	//	cout << student[i].studentNumber;
-//	//	cout << endl;
-//	//}
-//	for (int i = 0; i <=(row+1)*column; i++) {
-//		for (int j = 0; j <= line; j++) {
-//			if (i % (j + 1) != 0 && j != 0) {
-//				student Student[i][j];
-//				string temporaryStudentNumber;
-//				cin >> temporaryStudentNumber;
-//				strcpy(Student[i][j].studentNumber, temporaryStudentNumber.c_str());
-//				cout << Student[i][j].studentNumber;
-//				cout << endl;
-//			}
-//		}
-//	}
-//	return 0;
-//}
-
-#include <iostream>
-#include <cstring>
-
+﻿#include <iostream>
+#include <iomanip>
 using namespace std;
-
-struct student
-{
-	int coordinates_x;
-	int coordinates_y;
-	char studentName[100];
+struct student {
+public:
 	int studentNumber;
+	int coor_x;
+	int coor_y;
 };
-
 int main() {
-	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 5; j++) {
-			student student[i][j];
-			student[i][j].studentNumber = i + j;
-			cout << student[i][j].studentNumber;
-			cout << endl;
+	student str[13][7];  //使用12*6座位排布进行输出测试
+	for (int i = 0;i <= 12;i++) {
+		for (int j = 0;j <= 6;j++) {
+			if (i == 0 || i == 4 || i == 8 || i == 12 || j == 6) {
+				str[i][j].coor_x = 0;
+				str[i][j].coor_y = 0;
+			}
+			else {
+				str[i][j].coor_x = i;
+				str[i][j].coor_y = 6 - j;
+			}
+		}
+	}
+
+	for (int j = 0;j <= 6;j++) {
+		for (int i = 0;i <= 12;i++) {
+			cout << "(";
+			cout << setw(2) << str[i][j].coor_x;
+			cout << ",";
+			cout << setw(2) << str[i][j].coor_y;
+			cout << ")";
+
+			if (i == 12) cout << endl;
 		}
 	}
 	return 0;
