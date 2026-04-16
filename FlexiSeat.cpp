1e6c577bf4a5d1ffad4d3aa46ff60394315d8012
Tiiiiimeless <Hello_zzm@hotmail.com>
2026-04-17 00:07:09 +0800

    将struct改用class

--- FlexiSeat.cpp
+++ FlexiSeat.cpp
@@ -1,42 +1,29 @@
 ﻿#include <iostream>
 #include <iomanip>
+#include "struct_Student.h"
 
-using namespace std;
-
-struct student {
-public:
-	int studentNumber;
-	int coor_x;
-	int coor_y;
-};
+//如果将该结构体打包为.h头文件，因为无法简单的将二维结构体数组作为函参传入函数，所以将for循环放在外面，将结构体传入函数，进行下一步操作
 
 int main() {
-	student str[41][21]; //仿动态数组，且x_max为40，y_max为20
+	Student student[41][21]; //仿动态数组，且x_max为40，y_max为20
 	int r = 0, l = 0, g = 0; //r:行数，l:列数，g:间隔数
-	cout << "输入行列组" << endl;
-	cin >> r >> l >> g;
+	std::cout << "输入行列组" << std::endl;
+	std::cin >> r >> l >> g;
 	for (int i = 0;i <= g*(l+1);i++) {
-		for (int j = r-1;j >=0;j--) {
+		for (int j = r;j >=0;j--) {
 			if (i%(l+1)==0||j==0) {
-				str[i][j].coor_x = 0;
-				str[i][j].coor_y = 0;
+				student[i][j].setStudentCoordinates(0,0);
 			}
 			else {
-				str[i][j].coor_x = i;
-				str[i][j].coor_y = j;
+				student[i][j].setStudentCoordinates(i,j);
 			}
 		}
 	}
 
-	for (int j = r-1;j >= 0;j--) {
+	for (int j = r;j >= 0;j--) {
 		for (int i = 0;i <= g*(l+1);i++) {
-			cout << "(";
-			cout << setw(2) << str[i][j].coor_x;
-			cout << ",";
-			cout << setw(2) << str[i][j].coor_y;
-			cout << ")";
-
-			if (i == g * (l + 1)) cout << endl;
+			student[i][j].printStudentCoordinatesSystem(i,j);
+			if (i == g * (l + 1)) std::cout << std::endl;
 		}
 	}
 	return 0;
